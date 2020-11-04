7fb52d0953039de66a3a61f8386420579844e218
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-11-04 18:04:05 +0700

    add oauth

---
+++ components/OAuthSignInButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <span id="oauth-container"></span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { auth } from '@/lib/firebase/client'
+import firebase from 'firebase'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+
+export default Vue.extend({
+  name: 'OAuthSignInButton',
+  mounted() {
+    const ui =
+      firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth)
+    const uiConfig = {
+      signInSuccessUrl: '/profile',
+      signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    }
+    ui.start('#oauth-container', uiConfig)
+  },
+})
+</script>
